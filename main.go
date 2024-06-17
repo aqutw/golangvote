4dc3682a0fc6e92a738fb7c64ff3ca18a728445f
vant.lai <vant.lai@ogglobal.net>
2024-06-17 23:01:14 +0800

    feat: start GET /login w HTML and POST /login w JSON

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	g := gin.Default()
+	g.LoadHTMLGlob("tmpl/*")
 	g.GET("/login", func(context *gin.Context) {
 		fmt.Println("hihi")
+		context.HTML(http.StatusOK, "login.tmpl", nil)
+	})
+	g.POST("/login", func(context *gin.Context) {
+		context.JSON(http.StatusOK, map[string]int{
+			"name": 123, // note: MUST ending w ,
+		})
 	})
 
 	if err := g.Run(":8080"); err != nil {
